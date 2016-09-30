1136bcd1ac0967ba9df6a57deab7a57056f4beb1
Carlos <carlos.sierra.zgz@gmail.com>
2016-09-30 18:37:21 +0200

    primer commit

--- ejercicio01.php
+++ ejercicio01.php
@@ -1,1 +1,25 @@
-TODO:
+script llamado ej 01- cree un array con 5 elementos - inicializarlo 
+ <!DOCTYPE html>
+<html><head>
+        <meta charset="UTF-8">
+        <title>Ejercicio 01 - PHP</title>
+</head><body>
+<?php
+//inicializamos
+echo "En este array vamos a mostrar los distintos nombres <hr>";
+$array1[]="Carlos";
+$array1[]="Oscar";
+$array1[3]="Pedro";
+$array1[]="Rafael";
+$array1[6]="Sara";
+
+
+foreach ( $array1 as $nombre ){
+	echo '<tr></tr>' . $nombre;
+
+}
+
+var_dump($array1);
+
+?>
+</body></html>
--- server.php
+++ server.php
@@ -1,5 +1,7 @@
 <?php
 
 echo "<pre>";
-var_dump ($_SERVER);
-echo "</pre>";
+//var_dump ($_SERVER);
+echo "</pre>;
+
+echo "El método de la petición es" . $_SERVER ['REQUEST_METHOD'];
